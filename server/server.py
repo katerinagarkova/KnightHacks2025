6b2f92a50057c1845ef0460030034ee2d6a27500
Tal <tal.avital04@gmail.com>
2025-10-25 14:59:31 -0400

    merge

--- server/server.py
+++ server/server.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
+import json
 
 app = FastAPI()
 
-points = np.load("code/points_lat_long.npy")
+points = np.load("server/code/points_lat_long.npy")
 
-asset_partition = np.load("code/asset_indexes.npy")
-photo_partition = np.load("code/photo_indexes.npy")
+asset_partition = np.load("server/code/asset_indexes.npy")
+photo_partition = np.load("server/code/photo_indexes.npy")
 
 print(f"{asset_partition}\n{photo_partition}\n[{photo_partition[1] + 1} {asset_partition[0]}]\n{len(points)}")
 
-photos = points[photo_partition[0]: photo_partition[1]]
-waypoints = points[photo_partition[1]: asset_partition[0]]
-assets = points[asset_partition[0]: asset_partition[1]]
+
+
+points_partitions = {
+    "points": points.tolist(),
+    "assets_partition": asset_partition.tolist(),
+    "photo_partition": photo_partition.tolist()
+}
+
+points_partitions_json = json.dumps(points_partitions)
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,11 +31,4 @@ app.add_middleware(
 
 @app.get("/api/points")
 def get_points():
-    arr = np.load("code/points_lat_long.npy")
-    return arr.tolist()
-
-@app.get("/api/assets")
-def get_assets():
-    arr = np.load("code/asset_indexes.npy")
-    print(arr)
-    return arr.tolist()
+    return points_partitions_json
