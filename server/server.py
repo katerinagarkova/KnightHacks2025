81a28ae309ec9b5b2c74fd6006bec894a1941b70
Tal <tal.avital04@gmail.com>
2025-10-25 19:40:42 -0400

    refactor sending waypoints

--- server/server.py
+++ server/server.py
@@ -1,24 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import numpy as np
-import json
+
+import waypoints
 
 app = FastAPI()
-
-points = np.load("server/code/points_lat_long.npy")
-
-asset_partition = np.load("server/code/asset_indexes.npy")
-photo_partition = np.load("server/code/photo_indexes.npy")
-
-print(f"{asset_partition}\n{photo_partition}\n[{photo_partition[1] + 1} {asset_partition[0]}]\n{len(points)}")
-
-points_partitions = {
-    "points": points.tolist(),
-    "assets_partition": asset_partition.tolist(),
-    "photo_partition": photo_partition.tolist()
-}
-
-points_partitions_json = json.dumps(points_partitions)
 
 app.add_middleware(
     CORSMiddleware,
@@ -29,4 +14,4 @@ app.add_middleware(
 
 @app.get("/api/points")
 def get_points():
-    return points_partitions_json
+    return waypoints.get_points()
---
+++ server/waypoints.py
@@ -0,0 +1,21 @@
+import numpy as np
+import json
+
+points = np.load("code/points_lat_long.npy")
+
+asset_partition = np.load("code/asset_indexes.npy")
+photo_partition = np.load("code/photo_indexes.npy")
+
+print(f"{asset_partition}\n{photo_partition}\n[{photo_partition[1] + 1} {asset_partition[0]}]\n{len(points)}")
+
+points_partitions = {
+    "points": points.tolist(),
+    "assets_partition": asset_partition.tolist(),
+    "photo_partition": photo_partition.tolist()
+}
+
+points_partitions_json = json.dumps(points_partitions)
+
+def get_points():
+    global points_partitions_json
+    return points_partitions_json
