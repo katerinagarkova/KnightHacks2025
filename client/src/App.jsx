dec81dedc471262864c4772b49f4dcd1dce90625
katerinagarkova <katerina.garkova@gmail.com>
2025-10-26 02:28:28 -0400

    changed css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { MapContainer, TileLayer, Marker, Popup, CircleMarker, Polyline } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, CircleMarker, Polyline} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function App() {
@@ -56,7 +56,11 @@ function App() {
   const center = getCenter(allPoints);
 
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
+    <div style={{ background: "linear-gradient(90deg, #0097DA, #78C239)", height: "100vh", width: "100%" }}>
+      <header style={{ textAlign: "center", padding: "5px", color: "white" }}>
+        <h1 style={{fontSize: 50}}>Drone Flight Path Simulator</h1>
+        <p style={{fontSize: 25}}>▶ Simulate drone inspection missions ◀</p>
+      </header>
       <MapContainer center={center} zoom={12} style={{ height: "100%", width: "100%" }}>
         <TileLayer
           url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
@@ -68,29 +72,32 @@ function App() {
           console.log("Waypoint:", p);
 
             return (
-              <CircleMarker key={`w-${i}`} center={p} radius={2} color="purple">
+              <CircleMarker key={`w-${i}`} center={p} radius={2} color="blue">
                 <Popup>Waypoint #{i}</Popup>
               </CircleMarker>
           )
         })}
-        {/* {waypoints.length > 1 && <Polyline positions={waypoints} color="purple" weight={2} />} */}
+        {/* {waypoints.length > 1 && <Polyline positions={waypoints} color="blue" weight={2} />} */}
 
         {/* Photos */}
         {photos.map((p, i) => (
-          <CircleMarker key={`p-${i}`} center={p} radius={2} color="blue">
+          <CircleMarker key={`p-${i}`} center={p} radius={2} color="green">
             <Popup>Photo #{i}</Popup>
           </CircleMarker>
         ))}
-        {/* {photos.length > 1 && <Polyline positions={photos} color="blue" weight={3} />} */}
+        {/* {photos.length > 1 && <Polyline positions={photos} color="green" weight={3} />} */}
 
         {/* Assets */}
         {assets.map((p, i) => (
-          <CircleMarker key={`a-${i}`} center={p} radius={2} color="red">
+          <CircleMarker key={`a-${i}`} center={p} radius={2} color="black">
             <Popup>Asset #{i}</Popup>
           </CircleMarker>
         ))}
-        {/* {assets.length > 1 && <Polyline positions={assets} color="red" weight={3} />} */}
+        {/* {assets.length > 1 && <Polyline positions={assets} color="black" weight={3} />} */}
       </MapContainer>
+      <div style={{ background: "linear-gradient(90deg, #0097DA, #78C239)", textAlign: "center", marginTop: "10px", fontSize: "18px"}}>
+        <strong>Total Distance Traveled:</strong> {/*totalMiles.toFixed(2)} miles*/}
+      </div>
     </div>
   );
 }
